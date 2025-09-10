3ce23b8f72ba6750ec70411b24249c5a4cf18cd7
Kaleb Moctezuma <kaleb@MacBook-Air-de-Kaleb.local>
2025-09-10 15:27:24 -0600

    Navigability was added

---
+++ src/components/Simulador.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Simulador() {
+  return (
+    <div className="custom-box">
+      <div className="custom-content">
+        <h2 className="header-label">Simulador</h2>
+        <h3 className="subtitle-label">Aquí podrás simular tus inversiones.</h3>
+      </div>
+    </div>
+  );
+}
